4b10a0dad654363672b8f618a1e491eddad22343
Sanskar sharma <sanskarsharma7654@gmail.com>
2026-07-27 23:12:33 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,35 @@
+"use strict"; // treat all JS code as newer version
+
+// alert (3+3 )  // we are usin nodejs, not browser
+
+console.log ( 3 
+    + 3  )  // code readability should be high
+
+           
+console.log("Sanskar")  
+
+
+let name ="sanskar"
+let age = 19
+let isLoggedin = false 
+
+// number = 2 to power 53
+//bigint 
+//string = ""
+//boolean = true/false
+//null = standalone value
+// undefined
+//symbol = for unique
+
+
+
+//object 
+
+
+console.log (typeof"string")
+
+console.log (typeof age)
+
+console.log(typeof null) // objest
+
+console.log (typeof undefined) // undefined
