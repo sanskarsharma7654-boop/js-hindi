846b787969e452ab2a9ac246c247a65f9cda87f2
Sanskar sharma <sanskarsharma7654@gmail.com>
2026-07-27 22:34:57 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,22 @@
+const accountID = 14453
+let accountEmail = " sanskarsharma7654@gmail.com"
+var accountPassword = "12334"
+accountCity = "jaipur"
+let accountstate;
+let accountNo;
+
+accountEmail = "false@gmail.com"
+accountPassword = "4SHJHJHJAT"
+accountCity ="seohara"
+
+
+// accountID = 2  //not allowed
+console.log(accountID)
+
+/*
+prefer not use var 
+because of issue in block scope and functional socope
+*/
+
+
+console.table([accountEmail, accountID , accountPassword , accountCity,accountstate,accountNo])
