f9f2deb577ff8a8b2f60ad5a025d86b0cf56f491
Sanskar sharma <sanskarsharma7654@gmail.com>
2026-07-29 19:03:21 +0000

    conversion of datatype

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,26 @@
+// let score = "Sanskar"
+
+// console.log(typeof (score));
+// console.log(typeof score);
+
+// let valueinnumber = Number(score)
+// console.log(typeof valueinnumber)
+// console.log(valueinnumber)
+
+// "33" = 33
+// "33a"=NaN
+// true = 1 ; false = 0
+
+
+let isLoggedIn = "SAnskar";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 = trune , 0 = false
+// "" = false
+// "hitesh" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log( typeof stringNumber)
