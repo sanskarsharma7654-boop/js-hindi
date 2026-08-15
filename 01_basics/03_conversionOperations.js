987dbcad1673e910db04b73351f182904c0099ba
Sanskar sharma <sanskarsharma7654@gmail.com>
2026-08-15 07:39:30 +0000

    conversion operations

--- 01_basics/03_conversionOperations.js
+++ 01_basics/03_conversionOperations.js
@@ -14,7 +14,7 @@
 
 let isLoggedIn = "SAnskar";
 let booleanIsLoggedIn = Boolean(isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 = trune , 0 = false
 // "" = false
@@ -23,4 +23,41 @@ console.log(booleanIsLoggedIn);
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log( typeof stringNumber)
+// console.log( typeof stringNumber)
+
+// ***************OPERATORS***********
+let value =3 
+let negvalue = -value
+
+// console.log(negvalue)
+
+
+// console.log(2+2);
+// console.log(2/3);
+// console.log(2*3);
+// console.log(2**2);
+// console.log(2%3);
+
+
+let str1="hello"
+let str2 = " sanskar"
+let str3= str1 + str2
+// console.log(str3);
+  
+
+// console.log(1 + "2")
+// console.log("1"+ 2 +2 )
+// console.log(1+2+"2")
+
+ 
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+num1 =  num2 = num3 = 2+2
+
+
+let gamecounter = 100
+++gamecounter;
+console.log(gamecounter);
