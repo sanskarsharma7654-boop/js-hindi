b96eb121bd1b801861b28c6842924d5e632f13ef
Sanskar sharma <sanskarsharma7654@gmail.com>
2026-08-16 06:51:52 +0000

    datatype

---
+++ 01_basics/05_dataypeSummery.js
@@ -0,0 +1,28 @@
+// Primitive
+
+// 7 types : String , number, boolean , Null , undefined , Symbol , BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const BigNumber = 12572575735713757531n
+
+
+// Reference (non primitive)
+
+// Array, Object ,Functions
+
+const Heros = ['saktimaan' , 'naagraj' , 'sanskar']
+let myObj={
+    name: 'sanskar',
+    age: 22,
+}
