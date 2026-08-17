2d5c6124ae79237c7089f28e59be00fac57c916e
Sanskar sharma <sanskarsharma7654@gmail.com>
2026-08-17 19:30:55 +0000

    strings

--- 01_basics/06_strings.js
+++ 01_basics/06_strings.js
@@ -2,5 +2,27 @@ const name = "Alice";
 const repocount = 50
 // console.log(name + repocount + "value");
 
-console.log(`hello my name is ${name} and my repocount is ${repocount} `);
+// console.log(`hello my name is ${name} and my repocount is ${repocount} `);
+
+const gamename = new String ("sanskar-sh")
+
+// console.log(gamename[0]);
+// console.log(gamename.__proto__);
+// console.log(gamename.length);
+// console.log(gamename.toUpperCase());
+console.log(gamename.charAt(6));
+console.log(gamename.indexOf('r'));
+console.log(gamename.substring(2));
+
+const newString = gamename.substring(0,4)
+console.log(newString);
+
+const anotherString = gamename.slice(-8,4);
+console.log(anotherString);
+
+
+
+
+
+
 
