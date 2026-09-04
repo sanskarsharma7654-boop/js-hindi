4a041416f1ec7e8abee0f7a88476a23de20a2d40
Sanskar sharma <sanskarsharma7654@gmail.com>
2026-09-04 20:58:15 +0000

    scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,9 +5,50 @@ if(true){
     let a = 30
     const b = 40
     var c = 34
-    console.log("inner:",a);
+    // console.log("inner:",a);   
+}
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+function one (){
+    const username = "hitesh"
+
+    function two(){
+        const website="youtube"
+        console.log(username);  
+        
+    }
+    // console.log(website);      //error
+    two()    
+}
+// one()
+
+if (true){
+    const username = "sanskar"
+    if (username === 'sanskar'){
+        const nickname = " cheetahh"
+        // console.log(username  + nickname);
+        
+    }
+    // console.log(nickname);      //error
     
 }
-console.log(a);
-// console.log(b);
-console.log(c);
+// console.log(username);         //error
+
+//********************** intersting*******************/
+
+console.log(addone(5))
+function addone(num){
+    return num + 1
+}
+
+
+
+// console.log(addtwo(5));                 error bcz of the syntax of function
+const addtwo = function(num){
+    return num + 2
+}
+
+
+
