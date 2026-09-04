f89d86571ac8ff8d4f7e0905f3dd723ff5c89724
Sanskar sharma <sanskarsharma7654@gmail.com>
2026-09-04 19:50:24 +0000

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,13 @@
+// var c = 300
+let a = 300
+
+if(true){
+    let a = 30
+    const b = 40
+    var c = 34
+    console.log("inner:",a);
+    
+}
+console.log(a);
+// console.log(b);
+console.log(c);
