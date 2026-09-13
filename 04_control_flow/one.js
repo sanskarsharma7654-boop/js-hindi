7164bd9803102128ba7f8675eeb351d4f6ba3dfb
Sanskar sharma <sanskarsharma7654@gmail.com>
2026-09-13 09:20:54 +0000

    control flows

---
+++ 04_control_flow/one.js
@@ -0,0 +1,59 @@
+//IF
+const isUserLoggedIn = true
+const temp = 41
+
+// if (temp === 41){
+//     console.log("executed");
+// }else{
+// console.log("temp is greater then 50");
+// }
+
+// < , > , == , === , <= , >= , !=
+
+// const score = 200 
+// if (score > 100){
+//     const power = "fly"
+//     // console.log(`user power :${power}`);
+//     console.log("User power :",power);
+// }
+
+
+const balance = 1000
+// implicit scope
+// if (balance > 1000) console.log("test"),console.log("test2");
+// else console.log("beekaar");
+
+//NESTED IF ELSE
+
+// if (balance<500) {
+//     console.log("less than 500");
+// }else if (balance<750) {
+//     console.log("less than 750");   
+// }else if (balance<900) {
+//     console.log("less than 900");   
+// }else {
+//     console.log("less than 1200");   
+// }
+
+const usetLoggedIn = true
+const debitCard = true
+const loggedInFromGoogle = false
+const loggedInFromemail = true
+
+if (usetLoggedIn && debitCard && 2==2 ) {
+    console.log("allow to buy course");   
+}
+
+if (loggedInFromGoogle || loggedInFromemail || ) {
+    console.log("User logged in");
+    
+    
+}
+
+
+
+
+
+
+
+
