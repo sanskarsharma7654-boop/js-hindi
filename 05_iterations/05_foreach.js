5fc80d22419e6df8db1cb76dd0f48296e92e8db5
Sanskar sharma <sanskarsharma7654@gmail.com>
2026-09-20 08:53:19 +0000

    foreach

--- 05_iterations/05_foreach.js
+++ 05_iterations/05_foreach.js
@@ -1,4 +1,46 @@
-// for each
+// foreach
 const programming = ["js","ruby","cpp","java"]
 
-programming.forEach()
+// programming.forEach( function (val){
+//     console.log(val);   
+// })
+
+
+// programming.forEach((item)=>{
+//     console.log(item);
+// })
+
+
+// function aalu(item){
+//     console.log(item);
+// }
+// programming.forEach(aalu)
+
+
+// programming.forEach((item,index,arr)=>{
+//     console.log(index,item,arr);
+    
+// })
+
+//**************IMPORTANT***************
+
+
+const mycoding=[
+    {
+        langagename:"javascript",
+        langugefile:"js"
+    },
+    {
+        langagename:"c++",
+        langugefile:"cpp"
+    },
+    {
+        langagename:"python",
+        langugefile:"py"
+    }
+]
+
+mycoding.forEach((item)=>{
+    console.log(item.langugefile);
+    
+})
---
+++ 05_iterations/06_six.js
@@ -0,0 +1,5 @@
+const programming = ["js","ruby","cpp","java"]
+
+programming.forEach((item)=>{
+    console.log(item);
+})
